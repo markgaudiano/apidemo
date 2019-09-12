72388a0a6af5ef436576f73a910cedd748783d1d
Mark Gaudiano <markgaudiano@Marks-iMac.local>
2019-09-12 10:48:09 +0800

    test authentication

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
 
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTY4MjU0ODE4LCJleHAiOjE1NzA4NDY4MTh9.IlDmDemq3-Sc-0MlEk3rxnlqbwr1pQwcEx_I58cAnQE";
 class App extends React.Component {
   constructor() {
     super();
@@ -10,16 +13,41 @@ class App extends React.Component {
     };
   }
   componentDidMount() {
-    fetch("http://localhost:1337/users")
-      .then(result => result.json())
-      .then(resultJSON => {
-        this.setState({
-          users: resultJSON
-        });
+    axios
+      .get("http://localhost:1337/users", {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then(response => {
+        // Handle success.
+
+        this.setState({ users: response.data });
+      })
+      .catch(error => {
+        // Handle error.
+        console.log("An error occurred:", error);
+      });
+
+    axios
+      .post("http://localhost:1337/auth/local", {
+        identifier: "markgaudiano",
+        password: "09327815880"
+      })
+      .then(response => {
+        // Handle success.
+        console.log("Well done!");
+        console.log("User profile", response.data.user);
+        console.log("User token", response.data.jwt);
+      })
+      .catch(error => {
+        // Handle error.
+        console.log("An error occurred:", error);
       });
   }
   render() {
-    console.log(this.state);
+    console.log(this.state.users);
+
     return (
       <div
         style={{
@@ -29,11 +57,11 @@ class App extends React.Component {
           width: "100%"
         }}
       >
-        {this.state.users.map(user => (
-          <ul key={user.id}>
-            <li>{user.username}</li>
-          </ul>
-        ))}
+        <ul>
+          {this.state.users.map(user => (
+            <li key={user.id}>{user.username}</li>
+          ))}
+        </ul>
       </div>
     );
   }
---
+++ src/components/login.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export class Login extends Component {
+  componentDidMount() {
+    axios
+      .post("http://localhost:1337/auth/local", {
+        identifier: "markgaudiano",
+        password: "09327815880"
+      })
+      .then(response => {
+        // Handle success.
+        console.log("Well done!");
+        console.log("User profile", response.data.user);
+        console.log("User token", response.data.jwt);
+      })
+      .catch(error => {
+        // Handle error.
+        console.log("An error occurred:", error);
+      });
+  }
+  render() {
+    return <div />;
+  }
+}
